41c579dc27fdfab2b682078c039cbceab7da1b5d
Seyyyy <mysdc7@gmail.com>
2022-10-09 17:49:47 +0900

    create kanban dir

---
+++ src/Kanban/kanban.test.tsx
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import React from "react";
+// import Todo from "./index";
+
+/*
+カンバンアプリ
+Todo, In Progress, Doneの3状態があり、それぞれの状態に変更ができる。
+それぞれの状態につきタスクリストをもつ。
+タスクリストは下方向に積み重なっていくように追加される。
+タスクにはタイトル、説明、タスクの状態の状態を持つ。
+タスクに表示されるプルダウンによってTodo、In Progress, Doneの状態を変更できる。
+*/
+
+describe("カンバンアプリのテスト", () => {
+  test("Todo, In Progress, Doneの3状態を持つ", () => {});
+});
