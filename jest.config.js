c3de7b58031a204aa4c1e59d56a5ab0b20a52628
Seyyyy <mysdc7@gmail.com>
2022-10-14 05:30:48 +0900

    nodo_modulesにESMのライブラリがあってもテスト可能に設定

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 const config = {
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
+  transformIgnorePatterns: [
+    `/node_modules/(?!(somePkg)|react-dnd|dnd-core|@react-dnd)`,
+  ],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
